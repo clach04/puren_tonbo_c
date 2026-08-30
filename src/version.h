42ed58b21faeb6facd3dda47ac3e1def4b971199
clach04 <clach04@gmail.com>
2026-08-29 17:23:37 -0700

    Add VERSIONINFO file properties from version.h

---
+++ src/version.h
@@ -0,0 +1,14 @@
+/* Single source of truth for version/product strings.
+ * Included by tonbo.rc (VERSIONINFO) and by main.c About dialog (future).
+ */
+#define TONBO_VERSION_MAJOR 0
+#define TONBO_VERSION_MINOR 1
+#define TONBO_VERSION_PATCH 0
+
+#define TONBO_VERSION_STRING "0.1.0"
+#define TONBO_VERSION_STRING4 "0, 1, 0, 0"
+
+#define TONBO_PRODUCT_NAME "Puren Tonbo"
+#define TONBO_FILE_DESCRIPTION "Puren Tonbo encrypted plain text notes. https://github.com/clach04/puren_tonbo_c"
+#define TONBO_COPYRIGHT "2026 Chris Clark"
+#define TONBO_ORIGINAL_FILENAME "tonbo.exe"
